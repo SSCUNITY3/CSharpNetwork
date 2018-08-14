dd20661ac33b0ba6663850cdc0673b77febb699d
Unknown <m.harandev@gmail.com>
2018-08-14 16:59:56 +0530

    Form Added

--- AsynchronousClient/AsynchronousClient/Form1.Designer.cs
+++ AsynchronousClient/AsynchronousClient/Form1.Designer.cs
@@ -28,13 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ipAddressLabel = new System.Windows.Forms.Label();
+            this.portNumberLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ipAddressLabel
+            // 
+            this.ipAddressLabel.AutoSize = true;
+            this.ipAddressLabel.Location = new System.Drawing.Point(44, 55);
+            this.ipAddressLabel.Name = "ipAddressLabel";
+            this.ipAddressLabel.Size = new System.Drawing.Size(80, 17);
+            this.ipAddressLabel.TabIndex = 0;
+            this.ipAddressLabel.Text = "IP Address:";
+            // 
+            // portNumberLabel
+            // 
+            this.portNumberLabel.AutoSize = true;
+            this.portNumberLabel.Location = new System.Drawing.Point(32, 118);
+            this.portNumberLabel.Name = "portNumberLabel";
+            this.portNumberLabel.Size = new System.Drawing.Size(92, 17);
+            this.portNumberLabel.TabIndex = 1;
+            this.portNumberLabel.Text = "Port Number:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(575, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 118);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(575, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ConnectButton.FlatAppearance.BorderSize = 0;
+            this.ConnectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ConnectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectButton.ForeColor = System.Drawing.SystemColors.Info;
+            this.ConnectButton.Location = new System.Drawing.Point(332, 162);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(110, 32);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = false;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(35, 209);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(682, 162);
+            this.textBox3.TabIndex = 5;
+            // 
+            // sendButton
+            // 
+            this.sendButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.sendButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.sendButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.sendButton.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.sendButton.FlatAppearance.BorderSize = 0;
+            this.sendButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendButton.ForeColor = System.Drawing.SystemColors.Window;
+            this.sendButton.Location = new System.Drawing.Point(332, 391);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(110, 32);
+            this.sendButton.TabIndex = 6;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = false;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.portNumberLabel);
+            this.Controls.Add(this.ipAddressLabel);
+            this.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.RightToLeftLayout = true;
+            this.Text = "SSC Network Test";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label ipAddressLabel;
+        private System.Windows.Forms.Label portNumberLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button sendButton;
     }
 }
 
--- AsynchronousClient/AsynchronousClient/Form1.cs
+++ AsynchronousClient/AsynchronousClient/Form1.cs
@@ -16,5 +16,15 @@ namespace AsynchronousClient
         {
             InitializeComponent();
         }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.Write("something");
+        }
     }
 }
